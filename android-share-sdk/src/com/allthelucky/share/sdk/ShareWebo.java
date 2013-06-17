8110f993459162cdd988aa83a7a52a42f96c0f1b
savant pan <winfirm@163.com>
2013-06-17 22:54:37 +0800

    Update ShareWebo.java

--- android-share-sdk/src/com/allthelucky/share/sdk/ShareWebo.java
+++ android-share-sdk/src/com/allthelucky/share/sdk/ShareWebo.java
@@ -7,27 +7,49 @@ package com.allthelucky.share.sdk;
  * 
  */
 public class ShareWebo {
-	public static final int SINA = 0;
-	public static final String SINA_KEY = "1228343418";
-	public static final String SINA_SECRET = "1228343418";
-	public static final String SINA_AUTH_URL = "https://open.weibo.cn/oauth2/authorize";
-
-	public static final int TECENT = 1;
-	public static final String TECENT_KEY = "801263295";
-	public static final String TECENT_SECRET = "c04459e0802f19e213af7ac548984708";
-	public static final String TECENT_AUTH_URL = "https://open.t.qq.com/cgi-bin/oauth2/authorize";
-
 	/**
-	 * 微博授权回调地址（公用）
-	 */
-	public static final String CALLBACK_RUL = "http://h.qdone.net.cn/share/index.html";
+	 * 新浪微博
+	 * /
+	public static final int SINA = 0;
+	/**
+	 * 新浪微博应用ID
+	 * /
+	public static final String SINA_KEY = "1228343418";
+	/**
+	 * 新浪微博应用SECRET
+	 * /
+	public static final String SINA_SECRET = "1228343418";
+	/**
+	 * 新浪微博授权地址
+	 * /
+	public static final String SINA_AUTH_URL = "https://open.weibo.cn/oauth2/authorize";
 	/**
 	 * SINA API服务接口调用地址
 	 */
 	public static final String SINA_URL = "https://api.weibo.com/2/statuses/update.json";
 	/**
+	 * 腾讯微博微博
+	 * /
+	public static final int TECENT = 1;
+	/**
+	 * 腾讯微博应用ID
+	 * /
+	public static final String TECENT_KEY = "801263295";
+	/**
+	 * 腾讯微博应用SECRET
+	 * /
+	public static final String TECENT_SECRET = "c04459e0802f19e213af7ac548984708";
+	/**
+	 * 腾讯微博应用授权地址
+	 * /
+	public static final String TECENT_AUTH_URL = "https://open.t.qq.com/cgi-bin/oauth2/authorize";
+	/**
 	 * TECENT API服务接口调用地址
 	 */
 	public static final String TECENT_URL = "https://open.t.qq.com/api/t/add";
+	/**
+	 * 微博授权回调地址（公用）
+	 */
+	public static final String CALLBACK_RUL = "http://h.qdone.net.cn/share/index.html";
 
 }
